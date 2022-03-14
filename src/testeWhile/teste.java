5c9e1bb9650fa9825dac463566fbc9affa931fb8
piedrohammer <piiedrohammer@gmail.com>
2022-03-14 14:10:00 -0300

    Repetição

---
+++ src/testeWhile/teste.java
@@ -0,0 +1,33 @@
+package testeWhile;
+
+public class teste {
+
+    public static void main(String[] args) throws Exception {
+
+        // While (enquanto)
+        // While (<Condição>) {}
+        //int contador = 1;
+        /*while (contador <= 100){ // vai ser repetido tudo o que for verdade
+            System.out.println(contador);
+            contador++;
+        }
+
+        /*
+        do {
+
+        } while (<condição>);
+         */
+
+        /*do {
+            System.out.println(contador);
+            contador++;
+        } while (contador <= 100);
+        */
+
+        // for (<valorInicial>; <condição>; <passo>) {}
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+    }
+}
+
